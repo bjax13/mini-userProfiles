85f54db5576cebae9aa3ac8d384348e56ec162be
Bryan Jackson <bjack13@gmail.com>
2016-12-13 15:17:07 -0700

    finished miniproject walkthrough

--- controller.js
+++ controller.js
@@ -1,5 +1,15 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
+
     $scope.thisAppIsBroken = "This angular app is working";
-   
+
+    $scope.getUsers = function () {
+      $scope.users = mainService.getUsers();
+    }
+    $scope.toggleFavorite = function (index) {
+      this.toggleFavorite = mainService.toggleFavorite();
+    }
+
+    $scope.getUsers();
+
+    $scope.toggleFavorite = mainService.toggleFavorite;
 })
